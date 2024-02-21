a31c87f562c3a5fdbb640aa18e6b128fa4a08c8d
EmirlanDogdurbaev <jigit38@gmail.com>
2024-02-21 22:43:44 +0600

    added new elements for practick

--- src/components/module/Layout/Layout.jsx
+++ src/components/module/Layout/Layout.jsx
@@ -10,6 +10,50 @@ const Layout = () => {
       <section className={classes.card_cont}>
         <Cards />
       </section>
+      <section className={classes.about}>
+        <div className={classes.title}>
+          <h3>
+            НАШ КУРС – <span>ЭТО ПРАКТИКУМ</span>
+          </h3>
+          <p>ВЫ ПОЛУЧАЕТЕ РЕЗУЛЬТАТ СРАЗУ ВО ВРЕМЯ ОБУЧЕНИЯ</p>
+        </div>
+        <div className={classes.list_cont}>
+          <h5>ЗА 8 НЕДЕЛЬ ВЫ:</h5>
+          <div>
+            <img
+              src="https://static.tildacdn.com/tild3939-6535-4537-b863-643838396139/noun_Check_Mark_2.svg"
+              alt="icon"
+              width={37}
+              height={36}
+            />
+            <p>освоите востребованную профессию</p>
+          </div>
+          <div>
+            <img
+              src="https://static.tildacdn.com/tild3939-6535-4537-b863-643838396139/noun_Check_Mark_2.svg"
+              alt="icon"
+              width={37}
+              height={36}
+            />
+            <p>
+              найдете первых клиентов и начнете зарабатывать уже во время
+              обучения
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://static.tildacdn.com/tild3939-6535-4537-b863-643838396139/noun_Check_Mark_2.svg"
+              alt="icon"
+              width={37}
+              height={36}
+            />
+            <p>
+              подготовитесь к стажировке с реальным поставщиком и оформите
+              продающее резюме
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
