bf9339293fc40f5877ebfcf94cadc6d7dcc92d3f
EmirlanDogdurbaev <jigit38@gmail.com>
2024-02-22 11:37:49 +0600

    fixed accordions

--- src/components/Stage/Stage.jsx
+++ src/components/Stage/Stage.jsx
@@ -3,61 +3,34 @@ import classes from "./Stage.module.scss";
 
 const Stage = () => {
   const [activeIndex, setActiveIndex] = useState(false);
-  const [activeIndex2, setActiveIndex2] = useState(false);
-
 
   return (
     <div className={classes.Stage}>
       <h3>
         Ваш путь на обучении в <span>3 этапа</span>
       </h3>
-      <h5 onClick={() => setActiveIndex(!activeIndex)}>
+      <h5>
         <span>ЭТАП № 1:</span> Разберётесь как устроены маркетплейсы
       </h5>
 
-      <button
-        onClick={() => {
-          setActiveIndex(!activeIndex);
-        }}
-        className={activeIndex ? classes.expanded : ""}
-      >
+      <button onClick={() => setActiveIndex(!activeIndex)}>
         <h6>Как устроены маркетплейсы</h6>
         <p>lorem</p>
       </button>
 
-      {activeIndex && (
-        <div>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Perferendis provident soluta delectus repellat perspiciatis
-            architecto sed ipsa excepturi rem consequatur saepe, facilis velit
-            doloremque quod tenetur necessitatibus blanditiis reiciendis
-            tempore!
-          </p>
-        </div>
-      )}
-
-      <button
-        onClick={() => {
-          setActiveIndex2(!activeIndex2);
-        }}
-        className={activeIndex2 ? classes.expanded : ""}
-      >
-        <h6>Как устроены маркетплейсы</h6>
-        <p>lorem</p>
-      </button>
-
-      {activeIndex2 && (
-        <div>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Perferendis provident soluta delectus repellat perspiciatis
-            architecto sed ipsa excepturi rem consequatur saepe, facilis velit
-            doloremque quod tenetur necessitatibus blanditiis reiciendis
-            tempore!
-          </p>
-        </div>
-      )}
+      <div className={activeIndex ? classes.expanded : classes.btn}>
+        {activeIndex && (
+          <div className={activeIndex ? classes.expanded : classes.def}>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Perferendis provident soluta delectus repellat perspiciatis
+              architecto sed ipsa excepturi rem consequatur saepe, facilis velit
+              doloremque quod tenetur necessitatibus blanditiis reiciendis
+              tempore!
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
