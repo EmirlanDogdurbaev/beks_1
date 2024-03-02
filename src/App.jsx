6c1cafff47b266822f5d9f7efc453645d3585650
EmirlanDogdurbaev <jigit38@gmail.com>
2024-03-03 00:46:05 +0600

    delete unnecessary part code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from "react";
 import classes from "./App.module.scss";
 import Form from "./components/Form/Form";
 import Layout from "./components/module/Layout/Layout";
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/module/Home/Home";
 function App() {
-  const [showForm, setShowForm] = useState(false);
-  const toggleForm = () => {
-    setShowForm(!showForm);
-    console.log("Form toggled:", showForm);
-  };
   return (
     <div className={classes.App}>
       <Routes>
